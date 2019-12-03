c07eb4cf5fe6d1b8ab15be7c4cb4791594810bd2
David <contact@davidtwigger.com>
2019-12-03 13:28:53 +0100

    integrate angular material

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule
+  ],
+  exports: [
+    FlexLayoutModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule
+  ]
 })
 export class MaterialModule {}
